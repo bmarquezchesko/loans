4b772bad104f4508af69dac058eba2e0e0768197
bmarquezches <braiangabriel.marquezchesko@mercadolibre.com>
2021-02-25 17:12:22 -0300

    Add users in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -16,10 +16,16 @@ CREATE TABLE loans (
 );
 
 INSERT INTO users (first_name, last_name, email) VALUES
-('Lucas', 'Lopez', 'lucaslopez@gmail.com'),
-('Federico', 'Gomez', 'federicogomez@gmail.com'),
-('Leandro', 'Sanchez', 'leandrosanchez@gmail.com'),
-('Luciano', 'Perez', 'lucianoperez@gmail.com');
+('Lucas', 'Lopez', 'lucaslopez@test.com'),
+('Federico', 'Gomez', 'federicogomez@test.com'),
+('Leandro', 'Sanchez', 'leandrosanchez@test.com'),
+('Luciano', 'Perez', 'lucianoperez@test.com'),
+('Emmanuel', 'Suarez', 'emmanuelsuarez@test.com'),
+('Giannina', 'Rodriguez', 'gianninarodriguez@test.com'),
+('Diego', 'Martinez', 'diegomartinez@test.com'),
+('Franco', 'Amaya', 'francoamaya@test.com'),
+('Alejandro', 'Marquez', 'alejandromarquez@test.com'),
+('Agustin', 'Ibarra', 'agustinibarra@test.com');
 
 INSERT INTO loans (total, user_id) VALUES
 (2500, 1),
@@ -32,7 +38,13 @@ INSERT INTO loans (total, user_id) VALUES
 (1000, 2),
 (1500, 2),
 (2000, 2),
-(2500, 2),
-(3000, 2),
-(700, 3),
-(1000, 3);
+(100, 3),
+(200, 3),
+(15000, 4),
+(1000, 6),
+(10000, 7),
+(20000, 7),
+(30000, 7),
+(500, 8),
+(15000, 10),
+(15000, 10);
